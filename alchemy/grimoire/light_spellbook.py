4f246399b72334ed09bd72ea53cb1a46e26d5851
andjajas <adjajasaputra@gmail.com>
2026-09-20 21:25:24 +0200

    removed typo in alchemy/grimoire/light_spellbook.py

--- alchemy/grimoire/light_spellbook.py
+++ alchemy/grimoire/light_spellbook.py
@@ -1,5 +1,5 @@
 def light_spell_allowed_ingredients() -> list[str]:
-    return ["earth", "“air", "fire", "water"]
+    return ["earth", "air", "fire", "water"]
 
 
 def light_spell_record(spell_name: str, ingredients: str) -> str:
